81be32573a2dc6dbd286a38b881243bd1986aadd
Lucas Mallmann <lucasmallmann76@gmail.com>
2019-10-25 22:28:42 -0300

    feat: Added routes file

--- App.js
+++ App.js
@@ -1,19 +1,6 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+import Routes from './src/routes';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your cool!</Text>
-    </View>
-  );
+  return <Routes />;
 }
