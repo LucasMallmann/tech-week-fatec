04fb85f82269b8a63b244f7b3f2ff3b5ab19c8ef
Lucas Mallmann <lucas.ribeiro@climatempo.com.br>
2019-10-28 13:55:45 -0300

    added dishes page in routes

---
+++ src/pages/Dishes/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default function Dishes() {
+  return (
+    <View>
+      <Text>Dishes</Text>
+    </View>
+  );
+}
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,12 +1,22 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import PropTypes from 'prop-types';
+import { View, Text, TouchableOpacity } from 'react-native';
 
 import styles from './styles';
 
-export default function Home() {
+export default function Home({ navigation }) {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Home</Text>
+      <Text style={styles.title}>Home screen</Text>
+      <TouchableOpacity onPress={() => navigation.navigate('Dishes')}>
+        <Text>Navigate</Text>
+      </TouchableOpacity>
     </View>
   );
 }
+
+Home.propTypes = {
+  navigation: PropTypes.shape({
+    navigate: PropTypes.func,
+  }).isRequired,
+};
