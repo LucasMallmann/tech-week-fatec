12ee389188554f9c2142e743b1f35749504badae
Lucas Mallmann <lucasmallmann76@gmail.com>
2019-10-28 15:18:14 -0300

    search bar

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,22 +1,37 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { View, Text, TouchableOpacity } from 'react-native';
+import React, { useState } from 'react';
+// import PropTypes from 'prop-types';
+import { View, TextInput, TouchableOpacity, Text } from 'react-native';
 
 import styles from './styles';
 
-export default function Home({ navigation }) {
+export default function Home() {
+  const [restaurant, setRestaurant] = useState('');
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Home screen</Text>
-      <TouchableOpacity onPress={() => navigation.navigate('Dishes')}>
-        <Text>Navigate</Text>
-      </TouchableOpacity>
+      <View style={styles.form}>
+        <TextInput
+          onChangeText={text => setRestaurant(text)}
+          value={restaurant}
+          placeholder="Digite o restaurante"
+          autoCapitalize="none"
+          autoCorrect={false}
+          style={styles.input}
+        />
+        <TouchableOpacity>
+          <Text>Ok</Text>
+        </TouchableOpacity>
+      </View>
+
+      <Text style={{ fontSize: 18, fontWeight: 'bold' }}>
+        Restaurantes Tops
+      </Text>
     </View>
   );
 }
 
-Home.propTypes = {
-  navigation: PropTypes.shape({
-    navigate: PropTypes.func,
-  }).isRequired,
-};
+// Home.propTypes = {
+//   navigation: PropTypes.shape({
+//     navigate: PropTypes.func,
+//   }).isRequired,
+// };
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -1,12 +1,34 @@
 import { StyleSheet } from 'react-native';
+import { colors, metrics } from '../../styles';
 
 export default StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
+    backgroundColor: colors.lighter,
+    paddingHorizontal: metrics.basePadding,
+  },
+  form: {
+    paddingTop: metrics.baseMargin * 8,
+    paddingBottom: metrics.baseMargin * 1.5,
+    borderBottomWidth: 1,
+    borderBottomColor: colors.light,
+    flexDirection: 'row',
+    justifyContent: 'space-around',
     alignItems: 'center',
   },
-  title: {
-    fontSize: 20,
+  input: {
+    backgroundColor: colors.white,
+    flex: 1,
+    marginRight: metrics.baseMargin,
+    height: 34,
+    borderRadius: metrics.baseRadius,
+    paddingHorizontal: metrics.basePadding / 2,
+    paddingTop: metrics.basePadding / 2,
+    paddingBottom: metrics.basePadding / 2,
+  },
+  buttonIcon: {
+    fontWeight: 'bold',
+    fontSize: 16,
+    color: colors.darker,
   },
 });
